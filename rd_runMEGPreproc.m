69c45a18498989f864843c05dde3b90962d16574
karentian <9136901+karentian@users.noreply.github.com>
2020-03-20 09:27:58 -0400

    update runMEGPreproc with manual channel rejection

--- rd_runMEGPreproc.m
+++ rd_runMEGPreproc.m
@@ -3,9 +3,9 @@
 %% setup
 % exptDir = '/Local/Users/denison/Data/Cupcake/MEG';
 exptDir = '/Users/kantian/Dropbox/Data/Cupcake/MEG'; 
-exptDir = '/Volumes/purplab/EXPERIMENTS/1_Current_Experiments/Rachel/Cupcake/Cupcake_Aperture/MEG';
-sessionDir = 'R1507_20190725';
-fileBase = 'R1507_CupcakeAperture_7.25.19';
+% exptDir = '/Volumes/purplab/EXPERIMENTS/1_Current_Experiments/Rachel/Cupcake/Cupcake_Aperture/MEG';
+sessionDir = 'R1507_20200311/disk';
+fileBase = 'R1507_CupcakeAperture_3.11.20';
 
 dataDir = sprintf('%s/%s', exptDir, sessionDir);
 preprocDir = sprintf('%s/preproc', dataDir);
@@ -32,12 +32,29 @@ else
 end
 runs = 1:nRuns
 
+%% manually set bad channels
+
+load(sprintf('%s/prep/channels_rejected.mat',dataDir));
+
+if ~isempty(channels_rejected)
+    chRejChar = char(channels_rejected); % cell to char array
+    chRejChar = chRejChar(:,end-2:end); % extract ch numbers in last three char
+    chRej = [];
+    for iC = 1:size(chRejChar,1)
+        chRej(iC) = str2double([chRejChar(iC,1) chRejChar(iC,2) chRejChar(iC,3)]); % to double
+    end
+    % chRej = chRej'; % TA2
+    badChannels = chRej;
+else
+    badChannels = [];
+end
+
 %% run preproc for each run
 for iRun = 1:nRuns
     run = runs(iRun);
     runFile = sprintf('%s/%s', preprocDir, runFiles(iRun).name);
     % preprocFileName = rd_MEGPreproc(runFile, figDir);
-    preprocFileName = meg_preproc(runFile, figDir);
+    preprocFileName = meg_preproc(runFile, figDir,badChannels,'Cupcake');
 end
 
 %% combine run files into preprocessed sqd
